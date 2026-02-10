fe3886572eb3a932c4626e9e5b7c4c0a266b930f
Ankit <ankitrajdihara123@gmail.com>
2026-02-10 07:07:53 +0530

    patch&put

--- fastapi-backend/app/schemas/user_schema.py
+++ fastapi-backend/app/schemas/user_schema.py
@@ -1,6 +1,21 @@
 from pydantic import BaseModel
+from typing import Optional
 class Books(BaseModel):
     id:int
     title:str
     author:str
     publisher:str
+
+
+
+
+class UpdateBookPut(BaseModel):
+    title: str
+    author: str
+    price: float
+
+
+class UpdateBookPatch(BaseModel):
+    title: Optional[str] = None
+    author: Optional[str] = None
+    price: Optional[float] = None
